c67e873f0f1ef194dfc326ec5b7f2bfb86f724c0
xingyueguji <114712960+xingyueguji@users.noreply.github.com>
2023-04-21 14:39:13 -0500

    Add files via upload

--- cosmics.C
+++ cosmics.C
@@ -1,8 +1,10 @@
 #include <cmath>
 
-void cosmics( Int_t nrun=1310, Int_t nevent=0, Int_t replay = 20000, Int_t fullreplay = -1) {
-    
-    gStyle->SetOptTitle(0);
+void cosmics( Int_t nrun=1310, Int_t nevent=0, Int_t replay = 1000, Int_t fullreplay = 1) {
+    // if full replay : fullreplay = 1;
+    // if single event: nevent = event number, replay = 1, fullreplay != 1;
+    // if 20k replay: replay = 20k, fullreplay != 1;
+    gStyle->SetOptFit(3);
     // --------------------------------------------------------------------------------
     
     TFile *f = new TFile(Form("nps_eel108_%d.root", nrun));
@@ -18,7 +20,6 @@ void cosmics( Int_t nrun=1310, Int_t nevent=0, Int_t replay = 20000, Int_t fullr
     t->SetBranchAddress("Ndata.NPS.cal.fly.adcCounter",&NadcCounter);
     Double_t adcCounter[2000];
     t->SetBranchAddress("NPS.cal.fly.adcCounter",&adcCounter);
-    t->GetEntry(nevent);
     Double_t Amp[2000];
     t->SetBranchAddress("NPS.cal.fly.adcSampPulseAmp",&Amp);
     Double_t Int[2000];
@@ -32,19 +33,25 @@ void cosmics( Int_t nrun=1310, Int_t nevent=0, Int_t replay = 20000, Int_t fullr
     
     TH2F *mapping = new TH2F("mapping","NPS",30,0,30,36,0,36);
     TH1F *Waveform = new TH1F("Waveform","",82,0,82*4);
-    TF1 *fitFunction = new TF1("fitFunction", "gaus", 0,82*4);
+    
     TH2F *mappingAmp = new TH2F("mappingAmp","Amp Mean",30,0,30,36,0,36);
     TH2F *mappingInt = new TH2F("mappingInt","Integral Mean",30,0,30,36,0,36);
     TH2F *mappingPed = new TH2F("mappingPed","Ped Mean",30,0,30,36,0,36);
     TH2F *mappingPulsetime = new TH2F("mappingPulsetime","Pulse Time mean",30,0,30,36,0,36);
-    TH1D *SingleAmp = new TH1D("SingleAmp","SingleAmp",200,-10,50);
     
-    TCanvas *test = new TCanvas("test","test",800,800);
     TCanvas *Ampmean = new TCanvas("Ampmean","Amp",3200,3200);
     TCanvas *Intmean = new TCanvas("Intmean","integral",1600,1600);
     TCanvas *Pedmean = new TCanvas("Pedmean","Pedestal",1600,1600);
     TCanvas *Pulsetimemean = new TCanvas("Pulsetimemean","Pulse time",1600,1600);
     TCanvas *SingleAmpCan = new TCanvas("SingleAmpCan","single block amp",800,800);
+    
+    TCanvas *Ampsingleplot = new TCanvas("Ampsingleplot","",3200,3200);
+    TH1F *h_SingleBlockAmp[1080];
+    for (int n = 0; n < 1080; n++){
+        h_SingleBlockAmp[n] = new TH1F(Form("h_SingleBlockAmp_%d",n),"",50,0,50);
+    }
+    
+    
     Int_t nPMT;
     Int_t location;
     Int_t NSamp;
@@ -65,123 +72,102 @@ void cosmics( Int_t nrun=1310, Int_t nevent=0, Int_t replay = 20000, Int_t fullr
         mappingPulsetime->GetYaxis()->SetBinLabel(i, Form("%d", i - 1));
       }
     
-    //testing only for one event and mapping parameter----------------------------------
-    
-    /*for (int i = 0; i < 36; i++){
-        nPMT = adcCounter[i];
-        //cout << nPMT << "nPMT is " << endl;
-        for (int a = 0; a < NSampWaveForm; a++){
-            if (SampWaveForm[a] == nPMT){
-                location = a;
-                cout << "location = "<< location << endl;
-                break;
-            }
-        }
-        NSamp = SampWaveForm[location+1];
-        for (int n = 0; n < NSamp; n++){
-            SampADC = SampWaveForm[location+2+n];
-            Waveform->SetBinContent(n+1, SampADC);//cut on threshold
-        }
-        Waveform->Fit(fitFunction);
-        //FIXME: Find a way to determine the goodness of the fit and how the threshold is applied.
-        TimingArray[i] = fitFunction->GetParameter(1);
-        //cout << TimingArray[i] << endl;
-    }*/
-
-    /*
-    for (int i = 1; i <= mapping->GetNbinsY(); i++) {
-        mapping->GetXaxis()->SetBinLabel(i, Form("%d", 31 - i - 1));
-      }
-    for (int i = 1; i <= mapping->GetNbinsY(); i++) {
-        mapping->GetYaxis()->SetBinLabel(i, Form("%d", i - 1));
-      }*/
-    
     //----------------------------------------------------------------------------------
-    Double_t TotAmp[1080]; // 1080 in total, 0 - 1079
-    Double_t TotIntegral[1080];
-    Double_t TotPulseTime[1080];
-    Double_t TotPedestal[1080];
-    Double_t MeanAmp[1080]; // 1080 in total, 0 - 1079
-    Double_t MeanIntegral[1080];
-    Double_t MeanPulseTime[1080];
-    Double_t MeanPedestal[1080];
     
-    for (int i = 0; i < 1080; i++){
-        TotAmp[i] = 0;
-        TotIntegral[i] = 0;
-        TotPulseTime[i] = 0;
-        TotPedestal[i] = 0;
-        MeanAmp[i] = 0;
-        MeanIntegral[i] = 0;
-        MeanPulseTime[i] = 0;
-        MeanPedestal[i] = 0;
-    }
     int counter;
     Long64_t nentries = t->GetEntries();
     int fullreplay1;
+    
     if (fullreplay == 1){
         fullreplay1 = nentries;
     }
     else{
         fullreplay1 = replay;
+    }//determine if full replay or 20k
+    
+    
+    if(replay == 1 && fullreplay != 1){
+        t->GetEntry(nevent);
+        for (Int_t a=0; a<NadcCounter; a++){
+            counter = int(adcCounter[a]);
+            h_SingleBlockAmp[counter]->Fill(Amp[a]);
+        }
+    }
+    else{
+        for(Int_t i=0; i<fullreplay1; i++){
+            t->GetEntry(i);
+            for (Int_t a=0; a<NadcCounter; a++){
+                counter = int(adcCounter[a]);
+                h_SingleBlockAmp[counter]->Fill(Amp[a]);
+                //TotIntegral[counter] += Int[a];
+                //TotPulseTime[counter] += Pulsetime[a];
+                //TotPedestal[counter] += Ped[a];
+                
+                /*cout <<"Event Number is"<< i << "PMT number is" << counter << "Amp is" << Amp[counter] << endl;
+                cout << "total Amp is" << TotAmp[counter] << endl;*/
+            }
+        }
     }
     
-    int nblock = 371; // testing Amp value;
-    double totalamp = 0;
-    for(Int_t i=0; i<fullreplay1; i++){
-        t->GetEntry(i);
-        for (Int_t a=0; a<200; a++){
-            if (int(adcCounter[a]) == 0 && a > 0){
-                break;
+    
+
+    
+    int fitrangelower;
+    int fitrangehigher;
+    double meanofamp;
+    double fittedmeanamp[1080] = {0};
+    
+    for (int i = 0; i < 1080; i++){
+        if(replay == 1 && fullreplay != 1){
+            fittedmeanamp[i] = atof(Form("%.1f",h_SingleBlockAmp[i]->GetMean()));
+        }
+        else{
+            meanofamp = h_SingleBlockAmp[i]->GetMean();
+            cout << meanofamp << endl;
+            if (meanofamp < 5 && meanofamp > 0){
+                fitrangelower = 3;
             }
-            if (adcCounter[a] == 371){
-                SingleAmp->Fill(Amp[a]);
-                totalamp += Amp[a];
+            else if (meanofamp == 0){
+                continue;
             }
-            counter = int(adcCounter[a]);
-            TotAmp[counter] += Amp[a];
+            else{
+                fitrangelower = int(meanofamp) - 5;
+            }
+            fitrangehigher = int(meanofamp) + 10; //define fit range, fit the histogram
+            TF1 *fitFunction = new TF1("fitFunction", "gaus", fitrangelower, fitrangehigher); // FIXME: change lower limit
+            h_SingleBlockAmp[i]->Fit(fitFunction);
+            cout << h_SingleBlockAmp[i]->GetFunction("fitFunction")->GetParameter(1) << endl;
+            if (h_SingleBlockAmp[i]->GetEntries() > 100){ // exclude bad fit
+                fittedmeanamp[i] = atof(Form("%.1f",h_SingleBlockAmp[i]->GetFunction("fitFunction")->GetParameter(1)));
+            }
             
-            //TotIntegral[counter] += Int[a];
-            //TotPulseTime[counter] += Pulsetime[a];
-            //TotPedestal[counter] += Ped[a];
-            
-            /*cout <<"Event Number is"<< i << "PMT number is" << counter << "Amp is" << Amp[counter] << endl;
-            cout << "total Amp is" << TotAmp[counter] << endl;*/
-            
+        }
+    }
+    
+    
+    
+    for (int i=1; i<271; i++){
+        Ampsingleplot->Clear();
+        Ampsingleplot->Divide(2,2);
+        for (int j=1; j<5; j++){
+            Ampsingleplot->cd(j);
+            h_SingleBlockAmp[(i-1)*4 + j - 1]->Draw();
+        }
+        Ampsingleplot->SaveAs(Form("~/NPS/singleblockamp/canvas_%d.pdf",i));
+    }
+    
+    for(Int_t i=0; i<1080; i++){
+        if(fittedmeanamp[i] > 0){//some histogram has too less bin to do the fitting
+            mappingAmp->SetBinContent(30 - i % 30 , i/30 + 1, fittedmeanamp[i]);
         }
         
     }
     
-    /*for(Int_t i=0; i<1080; i++){
-        MeanAmp[i] = TotAmp[i]/ nentries;
-        //MeanIntegral[i] = TotIntegral[i]/ nentries;
-        //MeanPulseTime[i] = TotPulseTime[i]/ nentries;
-        //MeanPedestal[i] = TotPedestal[i]/ nentries;
-        MeanAmp[i] = atof(Form("%.1f", MeanAmp[i]));
-    }
-    
-    for(Int_t i=0; i<1080; i++){
-        cout << MeanAmp[i] << endl;
-        mappingAmp->SetBinContent(30 - i % 30 , i/30 + 1, MeanAmp[i]);
-    }*/
-    cout << totalamp << endl;
-    cout << totalamp / nentries << endl;
-    SingleAmpCan->Divide(1,1);
-    SingleAmpCan->cd(1);
-    SingleAmp->Draw();
-    SingleAmpCan->SaveAs("~/NPS/SingleblockAmp.pdf");
-    
-    
-    
-    
-    
-    /*Ampmean->cd();
+    Ampmean->cd();
     gPad->SetGrid(1,1);
     mappingAmp->SetMarkerColor(kBlack);
-    mappingAmp->SetLabelSize(0.01,"Z");
     mappingAmp->Draw("colztext");
-    Ampmean->SaveAs("~/NPS/test233.pdf");*/
-    
+    Ampmean->SaveAs("~/NPS/Ampmapping.pdf");
     
     
 }
