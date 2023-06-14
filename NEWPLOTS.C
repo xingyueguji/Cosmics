dd15b984889a20014656b830bb4281aa908e78db
xingyueguji <114712960+xingyueguji@users.noreply.github.com>
2023-06-13 21:49:00 -0400

    Add files via upload

--- NEWPLOTS.C
+++ NEWPLOTS.C
@@ -1,6 +1,6 @@
 #include <cmath>
 
-void NEWPLOTS( Int_t nrun=1370) {
+void NEWPLOTS( Int_t nrun=55) {
     // if full replay : fullreplay = 1;
     // if single event: nevent = event number, replay = 1, fullreplay != 1;
     // if 20k replay: replay = 20k, fullreplay != 1;
@@ -22,12 +22,16 @@ void NEWPLOTS( Int_t nrun=1370) {
     t->SetBranchAddress("NPS.cal.fly.adcCounter",&adcCounter);
     Double_t Pulsetime[2000];
     t->SetBranchAddress("NPS.cal.fly.adcSampPulseTime",&Pulsetime);
+    Double_t Amp[2000];
+    t->SetBranchAddress("NPS.cal.fly.adcSampPulseAmp",&Amp);
     
     //Histogram------------------------------------------------------------------------
     TH1D *TotalHit = new TH1D("TotalHit","TotalHit_Above_4mV",100,0,100);
     TH2D *TimeBlock = new TH2D("TimeBlock","Time_vs_Block",120,0,120,100,0,400);
     TH2D *TimeBlock_Zoom = new TH2D("TimeBlock_Zoom","Time_vs_Block",120,0,120,10,100,140);
     TH1D *TotalGoodHit = new TH1D("TotalGoodHit","Good Hit",100,0,100);
+    TH1D *GoodHitPerBlock = new TH1D("GoodHitPerBlock","GoodHitPerBlock",120,0,120);
+    TH2D *AmpBlock = new TH2D("AmpBlock","Amp_vs_Block",120,0,120,30,0,30);
     cout << "abc" << endl;
 
 
@@ -43,8 +47,10 @@ void NEWPLOTS( Int_t nrun=1370) {
         for(Int_t j=0; j<NadcCounter; j++){
             TimeBlock->Fill(adcCounter[j]+1, Pulsetime[j]);
             TimeBlock_Zoom->Fill(adcCounter[j]+1, Pulsetime[j]);
+            AmpBlock->Fill(adcCounter[j]+1,Amp[j]);
             if(Pulsetime[j]<= 130 or Pulsetime[j]>= 110){
                 counterofgoodhit += 1;
+                GoodHitPerBlock->Fill(adcCounter[j]+1);
             }
         }
         TotalGoodHit->Fill(counterofgoodhit);
@@ -71,6 +77,16 @@ void NEWPLOTS( Int_t nrun=1370) {
     TotHits->Clear();
     TotHits->cd();
     TotalGoodHit->Draw();
-    TotalGoodHit->SaveAs(Form("Total_Good_Hits_%i.pdf",nrun));
+    TotHits->SaveAs(Form("Total_Good_Hits_%i.pdf",nrun));
+
+    TotHits->Clear();
+    TotHits->cd();
+    GoodHitPerBlock->Draw();
+    TotHits->SaveAs(Form("Total_Good_Hits_Per_Block_%i.pdf",nrun));
+
+    TCanvas *Amp = new TCanvas("Amp","",1600,1600);
+    Amp->cd();
+    AmpBlock->Draw("colz");
+    Amp->SaveAs(Form("Amp_VS_Block_%i.pdf",nrun));
 
 }
